35eb0dc79582f7f41e7e4cd1312d2a8b7a4db034
LiJing <lijing@3-dj.com>
2012-10-12 23:29:57 +0800

    add boxes slide over

---
+++ src/ControlBoxButton.h
@@ -0,0 +1,37 @@
+//
+//  CBoxButton.h
+//  Kinect_3DJ
+//
+//  Created by LJ on 8/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Kinect_3DJ_ControlBoxButton_h
+#define Kinect_3DJ_ControlBoxButton_h
+
+#include "BoxButton.h"
+
+class CLoopBoxButton: public CBoxButton{
+public:
+    CLoopBoxButton( float centerX, float centerY, float centerZ, int boxSize,
+        float redVal, float greenVal, float blueVal, float alphaVal, string soundName)
+        :CBoxButton(centerX, centerY, centerZ, boxSize, redVal, greenVal, blueVal, alphaVal, soundName)
+    {}
+
+    bool collisionTest(ofPoint *pPoint);
+    bool collisionTest(ofPoint point);
+};
+
+
+class CEffectBoxButton: public CBoxButton{
+public:
+    CEffectBoxButton( float centerX, float centerY, float centerZ, int boxSize,
+        float redVal, float greenVal, float blueVal, float alphaVal, string soundName)
+        :CBoxButton(centerX, centerY, centerZ, boxSize, redVal, greenVal, blueVal, alphaVal, soundName)
+    {}
+
+    bool collisionTest(ofPoint *pPoint);
+    bool collisionTest(ofPoint point);
+};
+
+#endif
