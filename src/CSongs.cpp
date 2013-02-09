0bf04a9fbae220694070cf8b0263761b8f73900c
Tweek <tweek@ubuntu.(none)>
2013-02-09 18:30:31 +0800

    fix linux path issue

--- src/CSongs.cpp
+++ src/CSongs.cpp
@@ -31,7 +31,7 @@ bool CSongs::load( string path )
             return false;
         }else{
             ofLogVerbose("found song bundle:" + m_dir[i].getFileName());
-            if ( !m_dir[i].doesFileExist( m_dir[i].getAbsolutePath() + "\\init.3dj")){
+            if ( !m_dir[i].doesFileExist( m_dir[i].getAbsolutePath() + "/init.3dj")){
                 ofLogVerbose("Error, didn't find init.3dj in song bundle: " + m_dir[i].getFileName());                
                 ofSleepMillis(5000);
             }
@@ -55,7 +55,7 @@ void CSongs::unload()
 bool CSongs::getFirst(string& songName, mapEntity& mapSong)
 {
     m_cursor = 0;
-    string filePath = m_dir.getPath( 0 ) + "\\init.3dj";
+    string filePath = m_dir.getPath( 0 ) + "/init.3dj";
     ofLogVerbose("Loading:" + filePath);
     m_configFile.loadFromFile( filePath, mapSong, false );
     songName = mapSong["soundName"].value;
@@ -71,7 +71,7 @@ bool CSongs::getNext( string& songName, mapEntity& mapSong)
         m_cursor = 0;
     }
     
-    string filePath = m_dir.getPath( m_cursor ) + "\\init.3dj";
+    string filePath = m_dir.getPath( m_cursor ) + "/init.3dj";
     ofLogVerbose("Loading: " + filePath);
     m_configFile.loadFromFile( filePath, mapSong, false );
     songName = mapSong["soundName"].value;
@@ -81,7 +81,7 @@ bool CSongs::getNext( string& songName, mapEntity& mapSong)
 
 bool CSongs::getCurrent( string& songName, mapEntity& mapSong)
 {    
-    string filePath = m_dir.getPath( m_cursor ) + "\\init.3dj";
+    string filePath = m_dir.getPath( m_cursor ) + "/init.3dj";
     m_configFile.loadFromFile( filePath, mapSong, false );
     songName = mapSong["soundName"].value;
 
